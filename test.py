9c5d688ac294bf0c41649358131c4b3af6f4d55d
mintun-myo-2020 <89120804+mintun-myo-2020@users.noreply.github.com>
2022-09-05 01:17:06 +0800

    initial commit 2

--- test.py
+++ test.py
@@ -1,9 +1,23 @@
 from sre_constants import JUMP
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from os import environ
+import os
 from flask_cors import CORS
 import pymysql
 pymysql.install_as_MySQLdb()
 from datetime import datetime, timezone
+from dotenv import load_dotenv
 
+load_dotenv()
+
+host=os.getenv('HOST')
+port=3306
+dbname="skillsdb"
+user=os.getenv('USER')
+password=os.getenv('PASSWORD')
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{user}:{password}@{host}:{port}/{dbname}'
+
+
+print(host, user, password)
