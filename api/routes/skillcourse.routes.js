7ce13e0e04313188d20ed51d141bcb941cbd2bde
ishaa-m <isha09091@gmail.com>
2022-10-19 22:33:40 +0800

    chore: minor update

--- api/routes/skillcourse.routes.js
+++ api/routes/skillcourse.routes.js
@@ -1,10 +1,10 @@
 module.exports = app => {
-    const courseskill = require("../controllers/skillcourse.controller.js");
+    const skillcourse = require("../controllers/skillcourse.controller.js");
   
     var router = require("express").Router();
     
     // Create a new Tutorial
-    router.post("/", courseskill.create);
+    router.post("/", skillcourse.create);
     
-    app.use('/api/courseskill', router);
+    app.use('/api/skillcourse', router);
   };
