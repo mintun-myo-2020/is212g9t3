5c03ee470de83471c812a89ea90d7049f1039082
ishaa-m <isha09091@gmail.com>
2022-11-06 21:11:36 +0800

    chore: WIP

--- ui/js/vue.js
+++ ui/js/vue.js
@@ -39,7 +39,8 @@ const main = Vue.createApp({
             skilltoCourse: {
                 skillSkillId: '',
                 courseCourseId: ''
-            }
+            },
+            ljs:[]
         }
     },
 
